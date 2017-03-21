de167b3f71b64521f3a6c46e36db04621f26d560
Michael Heumann <michael@mce-business.de>
2017-03-22 00:22:56 +0100

    ensure support for Django < 1.10

--- bcm/models.py
+++ bcm/models.py
@@ -4,7 +4,12 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import (GenericRelation, GenericForeignKey)
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.urls import reverse
+# TODO: remove if Django versions < 1.10 are not supported anymore
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
+
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext as _
 from model_utils.models import TimeStampedModel
