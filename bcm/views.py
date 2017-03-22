29a0d40f7e0de26950e2250f9fab9a1f24b89092
Michael Heumann <michael@mce-business.de>
2017-03-22 02:52:27 +0100

    adds missing fields attribute

--- bcm/views.py
+++ bcm/views.py
@@ -8,56 +8,55 @@ from django.views.generic import (
 )
 
 from .models import (
-	Company,
-	Person,
+    Company,
+    Person,
 )
 
 
 class CompanyCreateView(CreateView):
-
     model = Company
+    fields = '__all__'
 
 
 class CompanyDeleteView(DeleteView):
-
     model = Company
 
 
 class CompanyDetailView(DetailView):
-
     model = Company
+    fields = '__all__'
 
 
 class CompanyUpdateView(UpdateView):
-
     model = Company
+    fields = '__all__'
 
 
 class CompanyListView(ListView):
-
     model = Company
+    fields = ('companyname', 'shortname')
 
 
 class PersonCreateView(CreateView):
-
     model = Person
+    fields = '__all__'
 
 
 class PersonDeleteView(DeleteView):
-
     model = Person
+    fields = '__all__'
 
 
 class PersonDetailView(DetailView):
-
     model = Person
+    fields = '__all__'
 
 
 class PersonUpdateView(UpdateView):
-
     model = Person
+    fields = '__all__'
 
 
 class PersonListView(ListView):
-
     model = Person
+    fields = '__str__'
