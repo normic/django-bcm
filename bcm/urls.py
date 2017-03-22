2ae07456213c2a8715fea4b8dd4627cb70f40a6b
Michael Heumann <michael@mce-business.de>
2017-03-22 02:52:45 +0100

    nicer urls

--- bcm/urls.py
+++ bcm/urls.py
@@ -1,58 +1,57 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     url(
-        regex="^Company/~create/$",
+        regex="^company/create/$",
         view=views.CompanyCreateView.as_view(),
-        name='Company_create',
+        name='company_create',
     ),
     url(
-        regex="^Company/(?P<pk>\d+)/~delete/$",
+        regex="^company/(?P<pk>\d+)/delete/$",
         view=views.CompanyDeleteView.as_view(),
-        name='Company_delete',
+        name='company_delete',
     ),
     url(
-        regex="^Company/(?P<pk>\d+)/$",
+        regex="^company/(?P<pk>\d+)/$",
         view=views.CompanyDetailView.as_view(),
-        name='Company_detail',
+        name='company_detail',
     ),
     url(
-        regex="^Company/(?P<pk>\d+)/~update/$",
+        regex="^company/(?P<pk>\d+)/update/$",
         view=views.CompanyUpdateView.as_view(),
-        name='Company_update',
+        name='company_update',
     ),
     url(
-        regex="^Company/$",
+        regex="^company/$",
         view=views.CompanyListView.as_view(),
-        name='Company_list',
+        name='company_list',
     ),
-	url(
-        regex="^Person/~create/$",
+    url(
+        regex="^person/create/$",
         view=views.PersonCreateView.as_view(),
-        name='Person_create',
+        name='person_create',
     ),
     url(
-        regex="^Person/(?P<pk>\d+)/~delete/$",
+        regex="^person/(?P<pk>\d+)/delete/$",
         view=views.PersonDeleteView.as_view(),
-        name='Person_delete',
+        name='person_delete',
     ),
     url(
-        regex="^Person/(?P<pk>\d+)/$",
+        regex="^person/(?P<pk>\d+)/$",
         view=views.PersonDetailView.as_view(),
-        name='Person_detail',
+        name='person_detail',
     ),
     url(
-        regex="^Person/(?P<pk>\d+)/~update/$",
+        regex="^person/(?P<pk>\d+)/update/$",
         view=views.PersonUpdateView.as_view(),
-        name='Person_update',
+        name='person_update',
     ),
     url(
-        regex="^Person/$",
+        regex="^person/$",
         view=views.PersonListView.as_view(),
-        name='Person_list',
+        name='person_list',
     ),
-	]
+]
