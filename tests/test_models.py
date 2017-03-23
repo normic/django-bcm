5074ba9fc79c3816e945e9ac407df56a6b58ac8a
Michael Heumann <michael@mce-business.de>
2017-03-23 01:28:15 +0100

    adds some tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -16,11 +16,33 @@ from bcm import models
 class TestCompany(TestCase):
 
     def setUp(self):
-        pass
+        self.company = models.Company.objects.create(
+            companyname='Test AG', shortname='Test_AG')
 
     def test_string_representation(self):
-        company = models.Company(companyname="Test AG")
-        self.assertEqual(str(company), company.companyname)
+        self.assertEqual(str(self.company), self.company.companyname)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.company.get_absolute_url(), '/company/1/')
+
+    def tearDown(self):
+        pass
+
+
+class TestPerson(TestCase):
+
+    def setUp(self):
+        self.person = models.Person.objects.create(
+            first_name="Max", last_name="Mustermann")
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.person), self.person.fullname)
+
+    def test_fullname(self):
+        self.assertEqual("Max Mustermann", self.person.fullname)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.person.get_absolute_url(), '/person/1/')
 
     def tearDown(self):
         pass
